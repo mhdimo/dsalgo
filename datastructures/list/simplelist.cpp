768e2077cca89c4b97ac48a678cdab3331a6d079
mhdimo <akahito@naver.com>
2023-04-27 17:07:30 +0200

    upload simple list definition

---
+++ datastructures/list/simplelist.cpp
@@ -0,0 +1,44 @@
+#include<iostream>
+
+struct node {
+    int data;
+    node* next= nullptr;
+};
+
+int main(){
+    node* n;
+    node* temp;
+    node* head;
+
+    n = new node;
+    n->data = 10;
+    temp = n;
+    head = n;
+
+    std::cout<<temp->data<<std::endl;
+
+    n = new node;
+    n->data = 20;
+    temp->next = n;
+    temp = temp->next;
+
+    std::cout<<temp->data<<std::endl;
+
+    n = new node;
+    n->data = 30;
+    temp->next = n;
+    temp = temp->next;
+
+    std::cout<<temp->data<<std::endl;
+
+    n = new node;
+    n->data = 40;
+    temp->next = n;
+    temp = temp->next;
+    n->next = NULL;
+    std::cout<<temp->data<<std::endl;
+    std::cout<<head->data<<std::endl;
+
+    return 0;
+}
+
